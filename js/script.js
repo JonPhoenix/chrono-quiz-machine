56110675ca272f85f685832a8c119f380320cf68
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-06 12:25:46 -0700

    Adding Timer

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 // Function 1: start Quiz
+function startQuiz() {
+
+}
 
 // Set the timer
+
 
 // Set first question / shuffle questions
 
 
 // Function 2: Select answer
+function selectAnswer() {
+    
+}
 
 // Answers true / false / modify timer
 
+
 // Go to next question
+
 
 // shuffle questions until time is over
 
 
 // Function 3: Show final score /  enter initials
+
 
 // show high scores / clear high scores and restart quiz

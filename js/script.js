d13033fd707ffa22d7102bf4a4ea34088e139ff8
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-06 14:45:25 -0700

    Scripting question/answers

--- js/script.js
+++ js/script.js
@@ -1,29 +1,87 @@
-// Function 1: start Quiz
+// Variables declaration
+let startButton =  document.getElementById('start-button');
+let questionDevice = document.getElementById('question-device');
+
+let questionElement =  document.getElementById('question');
+let answerButtonsElement = document.getElementById('answer-buttons');
+
+let shuffleQuestions, currentQuestion;
+
+// Function: start Quiz / shuffle questions
+
+startButton.addEventListener('click', startQuiz);
+
 function startQuiz() {
+    startButton.classList.add('hide');
+    questionDevice.classList.remove('hide');
+    shuffleQuestions = questionPool.sort(() => Math.random() - .5)
+    currentQuestion = 0
+
+    nextQuestion()
+
+}
+// Go to next question
+function nextQuestion() {
+    showQuestion(shuffleQuestions[currentQuestion]);
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        let button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
 
 }
 
-// Set the timer
-
-
-// Set first question / shuffle questions
-
-
-// Function 2: Select answer
+// Function: Select answer
 function selectAnswer() {
-    
+
 }
 
 // Answers true / false / modify timer
+let questionPool = [
+    {
+        question: 'Q: String values must be enclosed within _________ when being assigned to variables.',
+        answers: [
+            { text: '1. commas', correct: false },
+            { text: '2. curly brackets', correct: true },
+            { text: '3. quotes',  correct: false },
+            { text: '4. parentheses', correct: false},
+        ]
+    }
+]
 
 
-// Go to next question
 
+// Set the timer
 
-// shuffle questions until time is over
+// let totalSeconds = 60*10;
+// let countMin = parseInt(totalSeconds/60);
+// let countSec = parseInt(totalSeconds%60);
 
+// function quizTimer(){
+//     document.getElementById("quiz-timer")
+//     = 'Time left: ' + countMin + ' minutes ' + countSec + ' seconds';
+//     if (totalSeconds <=0) {
+//         setTimeout('document.quiz.submit()', 1);
+//     }
+//     else {
+//         totalSeconds = totalSeconds -1;
+//         countMin = parseInt(totalSeconds/60); 
+//         countSec = parseInt(totalSeconds%60);
+//         setTimeout("CheckTime()", 1000);
+//     }
+// }
 
-// Function 3: Show final score /  enter initials
+// Function: Show final score /  enter initials
 
 
 // show high scores / clear high scores and restart quiz

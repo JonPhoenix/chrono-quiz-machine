19faaa2221b594f8494e6b2b256ab358256b5b76
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-06 01:34:25 -0700

    pseudo-code

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Function 1: start Quiz
+
+// Set the timer
+
+// Set first question / shuffle questions
+
+
+// Function 2: Select answer
+
+// Answers true / false / modify timer
+
+// Go to next question
+
+// shuffle questions until time is over
+
+
+// Function 3: Show final score /  enter initials
+
+// show high scores / clear high scores and restart quiz

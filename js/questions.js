0ef087d4649d1a4fbb21a47c9a38d96875a708bb
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-07 02:21:42 -0700

    Including a second js file

---
+++ js/questions.js
@@ -0,0 +1,38 @@
+let questionPool = [
+    {
+        question: 'Q: String values must be enclosed within ______ when being assigned to variables.',
+        answers: [
+            { text: '1. commas', correct: false },
+            { text: '2. curly brackets', correct: false },
+            { text: '3. quotes',  correct: true },
+            { text: '4. parentheses', correct: false },
+        ]
+    },
+    {
+        question: 'Q: Arrays in JavaScript can be used to store ______.',
+        answers: [
+            { text: '1. numbers and strings', correct: false },
+            { text: '2. other arrays', correct: false },
+            { text: '3. booleans',  correct: false },
+            { text: '4. all of the above', correct: true },
+        ]
+    },
+    {
+        question: 'Q: The condition in ab if / else statement is enclosed within ______.',
+        answers: [
+            { text: '1. quotes', correct: false },
+            { text: '2. curly brackets', correct: false },
+            { text: '3. parentheses',  correct: true },
+            { text: '4. square brackets', correct: false },
+        ]
+    },
+    {
+        question: 'Q: Is it true or false ______?',
+        answers: [
+            { text: '1. it is true', correct: true },
+            { text: '2. super false', correct: false },
+            { text: '3. totally false',  correct: false },
+            { text: '4. very false', correct: false },
+        ]
+    },
+]

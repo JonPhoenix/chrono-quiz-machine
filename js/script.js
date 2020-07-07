254baa5dce710aa230398d06328fccf4a9f68ab0
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-07 12:02:09 -0700

    Adding Form and setTimeout

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ let correctSign = document.getElementById('correct-sign');
 let incorrectSign = document.getElementById('incorrect-sign');
 
 let questionDevice = document.getElementById('question-device');
+let scoreDevice = document.getElementById('score-device');
 
 let questionElement = document.getElementById('question');
 let answerButtonsElement = document.getElementById('answer-buttons');
@@ -26,6 +27,25 @@ function startQuiz() {
     shuffleQuestions = questionPool.sort(() => Math.random() - .5)
     currentQuestion = 0
     nextQuestion()
+
+    let startingMinutes = 1;
+    let time = startingMinutes * 60;
+    let countdownEl = document.getElementById('countdown');
+    setInterval(updateCountdown, 1000);
+    function updateCountdown() {
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds
+
+        countdownEl.innerHTML = `${minutes}:${seconds}`;
+        time--;
+    }
+    setTimeout (showScore, 60000)
+    function showScore() {
+        questionDevice.classList.add('hide'); 
+        scoreDevice.classList.remove('hide');
+    }
 }
 // Function: Go to next question / shuffle questions
 function nextQuestion() {
@@ -51,7 +71,7 @@ function showQuestion(question) {
 function resetState() {
     clearStatus(document.body);
     nextButton.classList.add('hide');
-    // correctSign.classList.add('hide');
+    correctSign.classList.add('hide');
     // incorrectSign.classList.add('hide');
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild (answerButtonsElement.firstChild);
@@ -78,10 +98,11 @@ function setStatus(element, correct) {
     clearStatus(element)
     if (correct) {
         element.classList.add('correct'); 
-        //correctSign.classList.remove('hide');
+        correctSign.classList.remove('hide');
     }
     else {
         element.classList.add('incorrect');
+        correctSign.classList.add('hide');
         //incorrectSign.classList.remove('hide');
     }
 }
@@ -95,26 +116,8 @@ let script = document.createElement('script');
 script.src = 'js/questions.js';
 document.head.appendChild(script)
 // Set the countdown / timer
-let startingMinutes = 1;
-let time = startingMinutes * 60;
-let countdownEl = document.getElementById('countdown');
-setInterval(updateCountdown, 1000);
-function updateCountdown() {
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds
 
-    countdownEl.innerHTML = `${minutes}:${seconds}`;
-    time--;
-}
-
-// var timeoutID = setTimeout(score, 60000);
-// console.log('Start');
-
-// function score() {
-//     console.log('Stop')
-// }
 
 
 // Function: Show final score /  enter initials

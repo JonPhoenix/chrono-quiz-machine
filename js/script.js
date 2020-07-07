2a67278a6118738c39525d8dc79e0268bbca721f
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-07 01:50:45 -0700

    Seeting Timer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 // Variables declaration
 let startButton = document.getElementById('start-button');
+let introQuiz = document.getElementById('intro');
 let nextButton = document.getElementById('next-button');
+
+let correctSign = document.getElementById('correct-sign');
+let incorrectSign = document.getElementById('incorrect-sign');
 
 let questionDevice = document.getElementById('question-device');
 
@@ -19,6 +23,7 @@ nextButton.addEventListener('click', () => {
 
 function startQuiz() {
     startButton.classList.add('hide');
+    introQuiz.classList.add('hide');
     questionDevice.classList.remove('hide');
     shuffleQuestions = questionPool.sort(() => Math.random() - .5)
     currentQuestion = 0
@@ -49,6 +54,9 @@ function showQuestion(question) {
 function resetState() {
     clearStatus(document.body);
     nextButton.classList.add('hide');
+    correctSign.classList.add('hide');
+    incorrectSign.classList.add('hide');
+
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild (answerButtonsElement.firstChild);
     }
@@ -76,14 +84,14 @@ function selectAnswer(e) {
 function setStatus(element, correct) {
     clearStatus(element)
     if (correct) {
-        element.classList.add('correct');
+        element.classList.add('correct'); 
+        //correctSign.classList.remove('hide');
     }
     else {
         element.classList.add('incorrect');
+        //incorrectSign.classList.remove('hide');
     }
 }
-
-
 
 function clearStatus(element) {
     element.classList.remove('correct')
@@ -98,7 +106,7 @@ let questionPool = [
             { text: '1. commas', correct: false },
             { text: '2. curly brackets', correct: false },
             { text: '3. quotes',  correct: true },
-            { text: '4. parentheses', correct: false},
+            { text: '4. parentheses', correct: false },
         ]
     },
     {
@@ -107,7 +115,7 @@ let questionPool = [
             { text: '1. numbers and strings', correct: false },
             { text: '2. other arrays', correct: false },
             { text: '3. booleans',  correct: false },
-            { text: '4. all of the above', correct: true},
+            { text: '4. all of the above', correct: true },
         ]
     },
     {
@@ -116,32 +124,47 @@ let questionPool = [
             { text: '1. quotes', correct: false },
             { text: '2. curly brackets', correct: false },
             { text: '3. parentheses',  correct: true },
-            { text: '4. square brackets', correct: false},
+            { text: '4. square brackets', correct: false },
         ]
-    }
+    },
+    {
+        question: 'Q: Is it true or false ______?',
+        answers: [
+            { text: '1. it is true', correct: true },
+            { text: '2. super false', correct: false },
+            { text: '3. totally false',  correct: false },
+            { text: '4. very false', correct: false },
+        ]
+    },
 ]
-
-
 
 // Set the timer
 
-// let totalSeconds = 60*10;
-// let countMin = parseInt(totalSeconds/60);
-// let countSec = parseInt(totalSeconds%60);
+let startingMinutes = 1;
+let time = startingMinutes * 60;
 
-// function quizTimer(){
-//     document.getElementById("quiz-timer")
-//     = 'Time left: ' + countMin + ' minutes ' + countSec + ' seconds';
-//     if (totalSeconds <=0) {
-//         setTimeout('document.quiz.submit()', 1);
-//     }
-//     else {
-//         totalSeconds = totalSeconds -1;
-//         countMin = parseInt(totalSeconds/60); 
-//         countSec = parseInt(totalSeconds%60);
-//         setTimeout("CheckTime()", 1000);
-//     }
+let countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
+
+// var timeoutID = setTimeout(score, 60000);
+// console.log('Start');
+
+// function score() {
+//     console.log('Stop')
 // }
+
+
 
 // Function: Show final score /  enter initials
 
